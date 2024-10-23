d50a3f36469fd67964ddaae4e7a8e5b47cce1d93
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-10-22 22:56:10 -0300

    commit

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -14,9 +14,9 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::create(['name' => 'Pão de forma', 'description' => 'pão branco em fatias','price' => 4.50, 'image' => 'images/Z3nRSYD95cdtZZcNV3qehhjptYRO5yRLsojKpnAH.jpg']);
-        Product::create(['name' => 'Pão caseiro', 'description' => 'pão branco macio','price' => 4.50, 'image' => 'images/b1XBRvo8qVAwfG4INPbzjTQQHwmHlzhRkFHqnrFH.jpg']);
-        Product::create(['name' => 'Brioche', 'description' => 'pão super macio','price' => 12.00, 'image' => 'images/F5GdxLtZA1TMnVqVCyuzD8SCQ7EKMAdY2hLNp3ey.jpg']);
+        Product::create(['name' => 'Pão de forma', 'description' => 'pão branco em fatias','price' => 4.50, 'image' => 'images/pao_forma.jpg']);
+        Product::create(['name' => 'Pão caseiro', 'description' => 'pão branco macio','price' => 4.50, 'image' => 'images/pao-de-forma-caseiro_02.jpg']);
+        Product::create(['name' => 'Brioche', 'description' => 'pão super macio','price' => 12.00, 'image' => 'images/pan-brioche-sledibrioche.jpg']);
     }
 
 }
