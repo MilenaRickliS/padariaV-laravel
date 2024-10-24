95fdb303eefb0a3fe219e6b44b1ee9feb7654321
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-10-24 14:58:19 -0300

    commit

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -14,9 +14,12 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::create(['name' => 'Pão de forma', 'description' => 'pão branco em fatias','price' => 4.50, 'image' => 'images/pao_forma.jpg']);
-        Product::create(['name' => 'Pão caseiro', 'description' => 'pão branco macio','price' => 4.50, 'image' => 'images/pao-de-forma-caseiro_02.jpg']);
-        Product::create(['name' => 'Brioche', 'description' => 'pão super macio','price' => 12.00, 'image' => 'images/pan-brioche-sledibrioche.jpg']);
+        Product::create(['name' => 'Pão de forma', 'description' => 'pão branco em fatias','price' => 4.50, 'image' => 'images/paoforma.png']);
+        Product::create(['name' => 'Pão caseiro', 'description' => 'pão branco macio','price' => 4.50, 'image' => 'images/caseiro.png']);
+        Product::create(['name' => 'Brioche', 'description' => 'pão super macio','price' => 12.00, 'image' => 'images/brioche.png']);
+        Product::create(['name' => 'Pão francês', 'description' => 'pão crocante','price' => 0.5, 'image' => 'images/frances.png']);
+        Product::create(['name' => 'Pão Italiano', 'description' => 'pão macio, ideal para sopa','price' => 15.00, 'image' => 'images/italiano.png']);
+        Product::create(['name' => 'Pão Integral', 'description' => 'pão preto com grãos','price' => 12.00, 'image' => 'images/integral.png']);
     }
 
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,29 @@
         @yield('content')
     </main>
     <footer>
-        
+        <div class="footer-flex">
+            <div class="footer-info">
+                <p><i class="bi bi-house-fill"></i> Endereço: Rua Bento Munhoz da Rocha Neto, n° 1028</p>
+                <p><i class="bi bi-telephone-fill"></i> Telefone: 42 99984134953</p>
+                <p><i class="bi bi-bag-check-fill"></i> Diversos tipos de pães! Faça o seu pedido!</p>
+            </div>
+            <div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1860954.8724583685!2d-51.68594295014845!3d-24.35772474143706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ef4fd942cd30a1%3A0x37922f66db5604a5!2sPanificadora%20Vit%C3%B3ria!5e0!3m2!1spt-BR!2sbr!4v1729787118906!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+        <div class="footer-flex2">
+            <div>
+                 <p class="bemvindo">Bem-vindo!</p>
+                    
+            </div>
+            <div>
+                <img src="./logo_da_padaria.png" alt="logo-padaria" width="100px" height="auto"/>
+            </div>
+            <div>
+                <p><i class="bi bi-envelope-fill"></i> panificadora_vitoria@hotmail.com</p>
+                <p><i class="bi bi-telephone-fill"></i> (42) 98413-4953</p>
+            </div>
+        </div>
     </footer>
 </body>
 </html>
