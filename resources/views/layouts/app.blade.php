19b8dd6b0b3c3cbd27de74a5d5ab7a70c1ffa3bf
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-11-06 21:49:10 -0300

    cc

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,31 +8,52 @@
     <link rel="stylesheet" href="{{ asset('estilo.css') }}">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <div class="menu">
-                    <li><a href="{{ route('products.cardapio') }}"><i class="bi bi-list"></i> Cardápio</a></li>
-                    <li><a href="{{ route('pedidos.index') }}"><i class="bi bi-bag-check-fill"></i> Minhas Compras</a></li>
-                </div>
-                <div class="menu">
-                    @if (Auth::check())
-                    <li>
-                        <form action="{{ route('logout') }}" method="POST" >
-                            @csrf
-                            <button class="logout" type="submit"><i class="bi bi-escape"></i> Logout</button>
-                        </form>
-                    </li>                    
-                    @else
-                        <li><a href="{{ route('login') }}"><i class="bi bi-person-circle"></i>  Login</a></li>
-                        <li><a href="{{ route('register') }}"><i class="bi bi-person-plus"></i> Register</a></li>
-                    @endif
-                </div>
+<header>
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="#">Panificadora Vitória</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('products.cardapio') }}">
+                        <i class="bi bi-list"></i> Cardápio
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('pedidos.index') }}">
+                        <i class="bi bi-bag-check-fill"></i> Minhas Compras
+                    </a>
+                </li>
+                @if (Auth::check())
+                <li class="nav-item">
+                    <form action="{{ route('logout') }}" method="POST" class="d-inline">
+                        @csrf
+                        <button class="btn btn-link nav-link logout" type="submit"><i class="bi bi-escape"></i> Logout</button>
+                    </form>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">
+                        <i class="bi bi-person-circle"></i> Login
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">
+                        <i class="bi bi-person-plus"></i> Register
+                    </a>
+                </li>
+                @endif
             </ul>
-        </nav>
-    </header>
+        </div>
+    </nav>
+</header>
     <main>
         @yield('content')
     </main>
