e54ec2eff6c5cc5d9380bc86e7258cede2634ecf
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-11-18 15:56:22 -0300

    cc

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $routeMiddleware = [
-        // outros middlewares
+        
         'auth' => \App\Http\Middleware\CheckLogin::class,
     ];
 }
